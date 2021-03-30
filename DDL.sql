255b39bb28f58c73bab07ce5f7c8186bf8035b39
racpsjcsp@gmail.com <racpsjcsp@gmail.com>
2021-03-30 10:10:37 -0300

    edit DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -7,17 +7,12 @@ create user 'user'@'localhost' identified by 'pass123';
 
 grant select, insert, delete, update on forum.* to user@'localhost';
 
-
-CREATE TABLE comentario (
+  CREATE TABLE usuario (
   id int(11) NOT NULL,
-  conteudo varchar(500) NOT NULL,
-  post int(11) DEFAULT NULL,
-  usuario int(11) DEFAULT NULL,
-  PRIMARY KEY (id),
-  KEY fk_post (post),
-  KEY fk_usuario2 (usuario),
-  CONSTRAINT fk_post FOREIGN KEY (post) REFERENCES post (id),
-  CONSTRAINT fk_usuario2 FOREIGN KEY (usuario) REFERENCES usuario (id)
+  nome varchar(50) NOT NULL,
+  email varchar(30) NOT NULL,
+  nickname varchar(20) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 
@@ -32,10 +27,14 @@ CREATE TABLE post (
 );
 
 
-CREATE TABLE usuario (
+CREATE TABLE comentario (
   id int(11) NOT NULL,
-  nome varchar(50) NOT NULL,
-  email varchar(30) NOT NULL,
-  nickname varchar(20) NOT NULL,
-  PRIMARY KEY (id)
+  conteudo varchar(500) NOT NULL,
+  post int(11) DEFAULT NULL,
+  usuario int(11) DEFAULT NULL,
+  PRIMARY KEY (id),
+  KEY fk_post (post),
+  KEY fk_usuario2 (usuario),
+  CONSTRAINT fk_post FOREIGN KEY (post) REFERENCES post (id),
+  CONSTRAINT fk_usuario2 FOREIGN KEY (usuario) REFERENCES usuario (id)
 );
